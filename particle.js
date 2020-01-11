48a6e642cab6232272a120e3dec904a464677215
ARSH SHARMA <184517@nith.ac.in>
2020-01-11 12:43:42 +0530

    add nn

--- particle.js
+++ particle.js
@@ -1,47 +1,80 @@
 class Particle {
-    constructor() {
-        this.pos = createVector(width/2, height/2);
+    constructor(x, y ) {
+        this.pos = createVector(x, y);
+        this.vel = createVector();
+        this.acc = createVector();
+        this.maxspeed = 3;
+        this.sight = 50;
         this.rays = [];
-        for(let a = 0; a < 360 ; a += 1) {
+        for(let a = 0; a < 360 ; a += 45) {
             this.rays.push(new Ray(this.pos, radians(a)));
 
         }
+        this.brain = new NeuralNetwork(this.rays.length, this.rays.length, 1);
     }
 
-    update(x, y)
+    applyForce(force){
+        this.acc.add(force);
+    }
+
+    update()
     {
-        this.pos.set(x, y)
+        this.pos.add(this.vel);
+        this.vel.add(this.acc);
+        this.vel.limit(this.maxspeed);
+        this.acc.set(0,0)
     }
 
     look(walls){
-         for(let ray of this.rays){
-            let record = Infinity; 
+
+        const inputs = [];
+         for(let i = 0;i < this.rays.length; i++ ){
+            const ray = this.rays[i];
+            let record = this.sight; 
             let closest = null;
             for(let wall of walls){
                 
                 const point = ray.cast(wall)
                 if(point){
                     const d = p5.Vector.dist(this.pos, point);
-                    if(d<record){
+                    if(d<record && d < this.sight){
                         record = d;
                         closest = point;
                     }
                     //record = min(d, record);
                     }
-        
+                    
                 }
+
+                // if(record < 2){
+                //     console.log("hit wall");
+
+                // }
+                inputs[i] = map(record, 0 , 50, 1, 0);
+                
+                
+
                 if(closest){
-                    stroke(255, 100)
+                    stroke(255, 100);
                 line(this.pos.x, this.pos.y, closest.x, closest.y);
                 }
 
-    }}
+    }
+                    const output = this.brain.predict(inputs);
+                    const angle = map(output[0], 0, 1, 0, TWO_PI);
+                    const steering = p5.Vector.fromAngle(angle);
+                    steering.setMag(this.maxspeed);
+                    steering.sub(this.vel);
+                    this.applyForce(steering);
+
+                    
+}
 
     show() {
-        fill(255);
-        ellipse(this.pos.x, this.pos.y, 4);
+        fill(255, 100);
+        ellipse(this.pos.x, this.pos.y, 8);
             for(let ray of this.rays){
-                ray.show();
+                //ray.show();
             }
         }
     }
--- sketch.js
+++ sketch.js
@@ -1,25 +1,37 @@
 let ray;
 let walls= [];  //boundary_obs
-let particle;
+let population = [];
 let xoff = 5000;
 let yoff = 100000;
+let start, end;
+
 function setup()
 {
+
+  tf.setBackend('cpu');
   createCanvas(1366, 768);
-  for(let i =0; i<5; i++){
-    let x1 = random(width);
-    let x2 = random(width);
-    let y1 = random(height);
-    let y2 = random(height);
-    walls[i] = new Boundary(x1, y1, x2, y2)
-  }
-  walls.push(new Boundary(0, 0 , width, 0));
-  walls.push(new Boundary(width, 0 , width, height));
-  walls.push(new Boundary(width, height , 0, height));
-  walls.push(new Boundary(0, height , 0, 0)); 
+  // for(let i =0; i<5; i++){
+  //   let x1 = random(width);
+  //   let x2 = random(width);
+  //   let y1 = random(height);
+  //   let y2 = random(height);
+  //   walls[i] = new Boundary(x1, y1, x2, y2)
+  // }
+  walls.push(new Boundary(50, height , 50, 200));
+  walls.push(new Boundary(50, 200 , 150, 50));
+  walls.push(new Boundary(150, 50 , height, 50));
+  walls.push(new Boundary(100, height , 100, 200));
+  walls.push(new Boundary(100, 200 , 175, 100));
+  walls.push(new Boundary(175, 100 , height, 100)); 
+  
+  start = createVector(75, 350);
+  end = createVector(350, 75);  
   // //ray = new Ray(100, 200)
   // wall = new Boundary(300,100,300,300);
-   particle = new Particle();
+  for(let i = 0; i < 100; i++){
+    population[i] = new Particle(start.x, start.y);
+  } 
+  
 }
 function draw()
 {
@@ -28,11 +40,17 @@ function draw()
   for(let wall of walls ){
   wall.show();
   }
-  particle.update(noise(xoff)*width, noise(yoff)*height);
-  particle.show();
+  for(let particle of population){
   particle.look(walls);
+  //particle.applyForce(createVector(0, -.1));
+  particle.update();
+  particle.show();
+  }
   xoff +=.01;
   yoff +=.01;
+  
+  //ellipse(start.x, start.y, 10);
+  ellipse(end.x, end.y, 10);
   // //ray.lookAt(mouseX, mouseY);
   // let point = ray.cast(wall);
   // //console.log(point);
